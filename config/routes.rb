55cbde83545f2ceb692380bcb5976dda477bbdf8
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-03-09 23:59:59 +0900

    deviseのコントローラ、ビュー、ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :groups
-  devise_for :users
+  devise_for :groups, controllers:{
+    registrations: "group/registrations",
+    sessions: "group/registrations"
+  }
+  
+  
+  devise_for :users, controllers:{
+    registrations: "user/registrations",
+    sessions: "user/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
