4343f7cb40f10543dc840fceef5a4613f4742247
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-03-29 23:59:59 +0900

    ユーザー、グループ、メッセージ、ユーザーグループ作成

--- db/migrate/20230421175203_devise_create_groups.rb
+++ db/migrate/20230421175203_devise_create_groups.rb
@@ -31,7 +31,7 @@ class DeviseCreateGroups < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
+      t.string :groupname
 
       t.timestamps null: false
     end
